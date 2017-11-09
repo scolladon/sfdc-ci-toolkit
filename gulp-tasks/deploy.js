9b896799c9bb0fefa322f744b908c23d674b26c2
Sébastien Colladon <scolladon@salesforce.com>
2017-11-09 16:17:04 +0100

    Write on the file system the deployment result

--- gulp-tasks/deploy.js
+++ gulp-tasks/deploy.js
@@ -3,10 +3,18 @@ const forceDeploy = require('../lib/plugins/gulp-jsforce-deploy');
 
 module.exports = (gulp, plugins,options) => {
   return cb => {
+    let status = null;
     gulp.src(options.src+'/**')
     .pipe(plugins.zip('pkg.zip'))
-    .pipe(forceDeploy(options).on('error',cb))
+    .pipe(forceDeploy(options).on('error', function(error) {
+      // we have an error
+      status = error;
+    }))
     .pipe(plugins.rename(DEPLOY_RESULT_FILE))
     .pipe(gulp.dest('.'));
+    console.log('test');
+    if(status) {
+      cb(status);
+    }
   };
 };
