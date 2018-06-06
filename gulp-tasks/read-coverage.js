dd1bcf1f2465fda6f061cf979fd63d4afcfb893e
BIEBER Baptiste ACN <baptiste.bieber@accenture.com>
2018-06-06 15:36:22 +0200

    If no test => no code coverage displayed
    If deployResult.numberTestsTotal = 0, show the code coverage bug then don't show the code coverage.

--- gulp-tasks/read-coverage.js
+++ gulp-tasks/read-coverage.js
@@ -15,24 +15,26 @@ module.exports = (gulp, plugins,options) => {
       return cb(options.repo + '/build' +DEPLOY_RESULT_FILE+ ' do not exist');
     }
     // Handle object instead of array for 1 element
-    let totalLine = 0, lineNotCovered = 0;
-    if(Array.isArray(deployResult.details.runTestResult.codeCoverage)) {
-      for(let j of deployResult.details.runTestResult.codeCoverage) {
-        totalLine += +j.numLocations;
-        lineNotCovered += +j.numLocationsNotCovered;
+    let totalLine = 0, lineNotCovered = 0, coverage = 0;
+    if(deployResult.numberTestsTotal > 0) {
+      if(Array.isArray(deployResult.details.runTestResult.codeCoverage)) {
+        for(let j of deployResult.details.runTestResult.codeCoverage) {
+          totalLine += +j.numLocations;
+          lineNotCovered += +j.numLocationsNotCovered;
+        }
+      } else {
+        totalLine += +deployResult.details.runTestResult.codeCoverage.numLocations;
+        lineNotCovered += +deployResult.details.runTestResult.codeCoverage.numLocationsNotCovered;
       }
-    } else {
-      totalLine += +deployResult.details.runTestResult.codeCoverage.numLocations;
-      lineNotCovered += +deployResult.details.runTestResult.codeCoverage.numLocationsNotCovered;
-    }
-    let coverage = ((totalLine > 0 ? (totalLine - lineNotCovered) / totalLine : 0) * 100).toFixed(2);
-    let att = [];
-    gutil.log(PLUGIN_NAME,'Code coverage: ' + coverage + '%');
-    if(deployResult.details.runTestResult.codeCoverageWarnings){
-      gutil.log(PLUGIN_NAME,'Warnings: ' + deployResult.details.runTestResult.codeCoverageWarnings.name + ': ' + deployResult.details.runTestResult.codeCoverageWarnings.message);
-    }
-    if(deployResult.details.runTestResult.failures){
-      gutil.log(PLUGIN_NAME,'Failures: ' + deployResult.details.runTestResult.failures.name + '.' + deployResult.details.runTestResult.failures.methodName + ': ' + deployResult.details.runTestResult.failures.message);
+      let coverage = ((totalLine > 0 ? (totalLine - lineNotCovered) / totalLine : 0) * 100).toFixed(2);
+      let att = [];
+      gutil.log(PLUGIN_NAME,'Code coverage: ' + coverage + '%');
+      if(deployResult.details.runTestResult.codeCoverageWarnings){
+        gutil.log(PLUGIN_NAME,'Warnings: ' + deployResult.details.runTestResult.codeCoverageWarnings.name + ': ' + deployResult.details.runTestResult.codeCoverageWarnings.message);
+      }
+      if(deployResult.details.runTestResult.failures){
+        gutil.log(PLUGIN_NAME,'Failures: ' + deployResult.details.runTestResult.failures.name + '.' + deployResult.details.runTestResult.failures.methodName + ': ' + deployResult.details.runTestResult.failures.message);
+      }
     }
   }
 };
