c0b2d08e53c9ae4af5ce6bd44c9e61458f820974
BIEBER Baptiste <baptiste.bieber@accenture.com>
2018-06-29 15:33:02 +0200

    Add the Script to retrieve metadata from Salesforce

--- gulp-tasks/retrieve.js
+++ gulp-tasks/retrieve.js
@@ -1,17 +1,17 @@
-const DEPLOY_RESULT_FILE = './retrieveResult.json';
+const RETRIEVE_RESULT_FILE = './retrieveResult.json';
 const retrieve = require('../lib/plugins/gulp-jsforce-retrieve');
 
 module.exports = (gulp, plugins, options) => {
   return cb => {
     let status = null;
-    // gulp.src(options.src+'/**')
-    // .pipe(plugins.zip('pkg.zip'))
+    gulp.src(options.src + '/package.xml')
     .pipe(retrieve(options).on('error', function(error) {
       // we have an error
       status = error;
     }))
-    .pipe(plugins.rename(DEPLOY_RESULT_FILE))
-    .pipe(gulp.dest('.'));
+    .pipe(plugins.rename(RETRIEVE_RESULT_FILE))
+    .pipe(gulp.dest('.'))
+    ;
     if(status) {
       cb(status);
     }
